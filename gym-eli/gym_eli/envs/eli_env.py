d7438c77edf1148840768bb99b10e44ffb4049f4
Eli Simhayev <elisimhayev@gmail.com>
2019-10-22 00:55:24 +0300

    Done train agent

--- gym-eli/gym_eli/envs/eli_env.py
+++ gym-eli/gym_eli/envs/eli_env.py
@@ -4,8 +4,8 @@ from gym.utils import seeding
 from .mc_dropout_utils import mc_dropout
 import numpy as np
 
-MAX_MC_DROPOUT_ITERATIONS = 10000
-
+MAX_MC_DROPOUT_ITERATIONS = 1000
+BATCH_SIZE = 1000
 
 class EliEnv(gym.Env):
     """
@@ -13,25 +13,31 @@ class EliEnv(gym.Env):
     """
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, net, confidence_rate, X_train, y_train, mc_dropout_iterations=50, mc_dropout_rate=0.5):
+    def __init__(self, net, confidence_rate, X_train, y_train, mc_dropout_rate=0.5):
         """
         :param net: keras network with 'set_mc_dropout_rate' function
         :param confidence_rate: confidence rate (uncertainty)
         :param mc_dropout_iterations: number of forward iterations used to estimate the uncertainty
         :param mc_dropout_rate: dropout rate
         """
+        print("in init")
         self.net = net
         self.confidence_rate = confidence_rate
         self.X_train = X_train
         self.y_train = y_train
-        self.mc_dropout_iterations = mc_dropout_iterations
+        self.data_shape = y_train.shape
         self.mc_dropout_rate = mc_dropout_rate
         ###TODO actions: agent can run 2 till mc_dropout_iterations iters.
-        self.action_space = spaces.Discrete(mc_dropout_iterations)
+        self.action_space = spaces.Discrete(MAX_MC_DROPOUT_ITERATIONS-1)
         ###TODO obs: numbers, probs. run T mc_iters, get accuracy of that ran for your batch.
-        self.observation_space = spaces.Box(low=0, high=1, shape=self.y_train.shape)
+        self.observation_space = spaces.Box(low=0, high=1, shape=self.data_shape)
         ###TODO minus num_iters for wrong, and 0 for right.
         self.reward_range = (-MAX_MC_DROPOUT_ITERATIONS, +MAX_MC_DROPOUT_ITERATIONS)
+        
+        
+        self.num_episodes = 0
+        self.curr_observation = np.zeros(self.data_shape)
+        self.curr_mc_dropout_iterations = 2
 
     def step(self, action):
         """
@@ -44,32 +50,37 @@ class EliEnv(gym.Env):
         # you got an error. the reward will be:
         #               (correct/batch) * mc_dropout_iters
         ### One episode = one epoch (one pass over all data)
-
-        y_mc_dropout, err_mc_dropout = self._take_action(action)
-        reward = None  # TODO reward: think about it. something with the error.
+        print("in step")
+        self.curr_mc_dropout_iterations = action+2
+        y_mc_dropout, err_mc_dropout = self._take_action(action+2)
+        reward = (1-err_mc_dropout) * self.data_shape[0] # TODO reward: think about it. something with the error.
         done = True
         info = {}
+        self.curr_observation = y_mc_dropout
+        self.num_episodes += 1
         return y_mc_dropout, reward, done, info
 
     def reset(self):
-        pass
+        print("in reset")
+        return self.curr_observation
         # return an initial observation of the env. But what obs I need to return after episode end?
         # ??????
 
     def render(self, mode='human', close=False):
-        pass
+        print(f"num_episodes = {self.num_episodes}")
+        print(f"mc_dropout_iterations = {self.curr_mc_dropout_iterations}")
 
     def _take_action(self, action):
         """
-        :param action: dict with 'batch_size' and 'mc_dropout_iterations'
+        :param action: number (mc_dropout_iterations)
         :return: y_mc_dropout, error
         """
-        batch_size = action['batch_size']
-        mc_dropout_iterations = action['mc_dropout_iterations']
+        print("in take action")
+        print(f"action={action}")
         y_mc_dropout, mc_uncertainty = mc_dropout(self.net, self.X_train,
-                                                  batch_size,
+                                                  BATCH_SIZE,
                                                   self.mc_dropout_rate,
-                                                  mc_dropout_iterations)
+                                                  action)
         err = self._compute_error(self.y_train, y_mc_dropout)
         return y_mc_dropout, err
 
--- gym-eli/setup.py
+++ gym-eli/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_eli',  # package name. used for import your environment (import gym_eli)
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['gym']  # TODO: maybe add here 'tqdm', and other stuff you use
       )
